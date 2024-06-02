b04308fb38dd561df6337fee8b2c0e34e8d9cb05
Mauro Vieira <mauroccvieira@gmail.com>
2024-06-02 16:08:45 -0300

    ci: fixes app paths used on deployment workflow

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/react-e2e-testing/",
   preview: {
     port: 8080,
     strictPort: true,
